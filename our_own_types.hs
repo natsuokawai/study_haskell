b7afe8a91f51bdbf5f9864a9ce332e03893c5ca0
natsuokw <kawai.natsuo@gmail.com>
2020-02-17 08:34:59 +0900

    update own_data_types

--- our_own_types.hs
+++ our_own_types.hs
@@ -16,3 +16,42 @@ data Shape' = Circle' Point Float | Rectangle' Point Point deriving (Show)
 surface' :: Shape' -> Float
 surface' (Circle' _ r) = pi * r ^ 2
 surface' (Rectangle' (Point x1 y1) (Point x2 y2)) = (abs $ x2- x1) * (abs $ y2 - y1)
+
+-- nudges
+nudge :: Shape' -> Float -> Float -> Shape'
+nudge (Circle' (Point x y) r) a b = Circle' (Point (x+a) (y+b)) r
+nudge (Rectangle' (Point x1 y1) (Point x2 y2)) a b = Rectangle' (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
+
+-- record type
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String
+                     } deriving (Show)
+
+data Car = Car {company :: String, model :: String, year :: Int} deriving (Show)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
