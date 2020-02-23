54697a1a1fc4642d409ae19413ee7815887bf4b0
natsuokw <kawai.natsuo@gmail.com>
2020-02-23 16:02:40 +0900

    update functinally_solving_problems

--- functinally_solving_problems.hs
+++ functinally_solving_problems.hs
@@ -12,3 +12,27 @@ solveRPN = head . foldl foldingFunction [] . words
         foldingFunction (x:xs) "ln"  = log x:xs
         foldingFunction xs "sum"     = [sum xs]
         foldingFunction xs numberString = read numberString:xs
+
+
+-- Heathrow to London
+data Node = Node Road (Maybe Road)
+data Road = Road Int Node
+data Section = Section { getA :: Int, getB :: Int, getC :: Int } deriving (Show)
+type RoadSystem = [Section]
+data Label = A | B | C deriving (Show)
+type Path = [(Label, Int)]
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) =
+  let priceA = sum $ map snd pathA
+      priceB = sum $ map snd pathB
+      forwardPriceToA = priceA + a
+      crossPriceToA = priceB + b + c
+      forwardPriceToB = priceB + b
+      crossPriceToB = priceA + a + c
+      newPathToA = if forwardPriceToA <= crossPriceToA
+                   then (A,a):pathA
+                   else (C,c):(B,b):pathB
+      newPathToB = if forwardPriceToB <= crossPriceToB
+                   then (B,b):pathB
+                   else (C,c):(A,a):pathA
+  in (newPathToA, newPathToB)
